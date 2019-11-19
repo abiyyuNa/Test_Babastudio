24ffb2087629a1eac7924b1ac863792700b531cd
abiyyuNa <42411982+abiyyuNa@users.noreply.github.com>
2019-11-19 09:46:23 +0700

    Add files via upload

---
+++ tugas/css/index.php
@@ -0,0 +1,11 @@
+<div>
+	<h2>Wellcome</h2>
+
+	<p align="justify"><img src="images/city_images/gopego_abu_dhabi.jpg" width="250" align="left" hspace="10">Diakui  sebagai museum  listrik   dan  energi baru   kelas dunia  yang bertumbuh kembang, unggul, dan terpercaya serta menjadi museum yang modern, inovatif dan dicintai masyarakat sebagai wahana rekreasi edukatif  yang berorientasi masa depan dengan bertumpu pada  potensi insani.</p>
+	<br><br><br>
+	<p align="justify"><img src="images/city_images/gopego_london.jpg" width="250" align="left" hspace="10"> Menjadikan MLEB sebagai tempat bermain dan  belajar yang menyenangkan melalui koleksi peraga, kegiatan dan fasilitas yang inovatif............. </p>
+	<br><br><br><br>
+	<p align="justify"><img src="images/city_images/gopego_yokohama.jpg" width="250" align="right" hspace="10"> Mewujudkan MLEB sebagai duta  pengetahuan di bidang ketenagalistrikan  dan   energi  baru   terbarukan  dalam upaya membangun karakter bangsa yang hemat energi.</p>
+	<br><br><br><br>
+	<p align="justify"><img src="images/city_images/Helsinki,Finlandia.jpg" width="250" align="right" hspace="10"> Turut    mencerdaskan   masyarakat   melalui  kegiatan edukatif   dengan melakukan sinergi bersama  instansi / perusahaan / lembaga terkait dan  memposisikan MLEB sebagai  sarana  informasi perkembangan sejarah  dan teknologi ketenagalistrikan  dan  energi  baru  terbarukan yang ramah lingkungan</p>
+</div>
